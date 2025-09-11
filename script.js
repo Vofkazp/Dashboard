2b2349c0e0fb95358837d51d58cae695b0ca7fae
Vofka <vofkazp@gmail.com>
2025-09-11 16:14:44 +0300

    sidebar

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+const body = document.body;
+const minimized = document.getElementById("minimized");
+const theme = document.getElementById("theme");
+const menuItems = document.querySelectorAll('.m-item');
+
+minimized.addEventListener("click", () => {
+  body.classList.toggle("min");
+});
+
+theme.addEventListener("click", () => {
+  if(body.classList.contains("dark")) {
+    body.classList.remove("dark");
+    body.classList.add("light");
+  } else {
+    body.classList.remove("light");
+    body.classList.add("dark");
+  }
+});
+
+menuItems.forEach((item) => {
+  item.addEventListener("click", () => {
+    menuItems.forEach((elem) => {
+      elem.classList.remove('active');
+    });
+    item.classList.add('active');
+  });
+});
